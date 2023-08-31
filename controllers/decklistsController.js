8b1295a6c61fbfb17be8d27c2e7b1ca20b747488
Justin Lau <justinlau094@hotmail.com>
2023-08-31 14:21:05 -0400

    send decklist obj in response

--- controllers/decklistsController.js
+++ controllers/decklistsController.js
@@ -64,13 +64,13 @@ const activeDecklist = async (req, res) => {
             .where({ 'decklist_cards.decklist_id': req.params.decklistId })
             .select('cards.*');
 
-        const pullCardIds = await knex('decklist_cards').where({ 'decklist_cards.decklist_id': req.params.decklistId }).select('decklist_id', 'card_id');
+        const pullCardIds = await knex('decklist_cards').where({ 'decklist_cards.decklist_id': req.params.decklistId }).select('*');
         const pullCardTypesPromises = pullCardIds.map(async (card) => {
             const types = await knex('types')
                 .join('card_types', 'types.id', '=', 'card_types.type_id')
                 .where({ 'card_types.card_id': card.card_id })
                 .select('types.name')
-                console.log("types", types)
+                // console.log("types", types)
             card.types = types.map((typeObj) => typeObj.name)
             return card
         })
@@ -89,13 +89,19 @@ const activeDecklist = async (req, res) => {
         pullCards = pullCards.map((card) => {
             const typesIndex = pullCardTypes.findIndex((typeObj) => typeObj.card_id === card.id)
             card.types = pullCardTypes[typesIndex].types
-            console.log("typesIndex", typesIndex)
+            // console.log("typesIndex", typesIndex)
             const colorIdIndex = pullCardColorIds.findIndex((colorObj) => colorObj.card_id === card.id)
             card.colorIdentity = pullCardColorIds[colorIdIndex].colorIdentity
             return card
         })
-        console.log("pullCards", pullCards)
-        res.status(200).json(pullCards);
+        // console.log("pullCards", pullCards)
+
+        let decklist = {};
+        const decklistInfo = await knex('decklists').where({ 'decklists.id': req.params.decklistId }).select('*')
+        decklist.name = decklistInfo[0].name;
+        decklist.cards = pullCards;
+
+        res.status(200).json(decklist);
     } catch (err) {
         res.status(500).send(`Error retrieving decklists: ${err}`)
     }
