1e50774ceb9db50ac3e6aa6a4bfcfe0f4d3ff45a
Justin Lau <justinlau094@hotmail.com>
2023-10-13 14:08:59 -0400

    enable cors for only deckrocket.net

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const decklistsRouter = require('./routes/decklists');
 const cardSearchRouter = require('./routes/cardSearch');
 const userRoutes = require("./routes/users");
 
-app.use(cors());
+app.use(cors(`https://deckrocket.net`));
 app.use(express.json());
 app.use(express.static('./public'));
 
